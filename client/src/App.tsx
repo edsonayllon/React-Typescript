1244d8d88e4e8e1ad07909a2554684b5ffb9f7f9
Edson Ayllon <EAYLLON1@GMAIL.COM>
2019-08-11 03:34:18 +0000

    testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const [count, setCount] = useState(3);
+  console.log(count);
   return (
     <div className="App">
       <header className="App-header">
